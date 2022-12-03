17b901da3ac01f98178286fb05c4137635194334
wanhwi.son <yeshtml5@gmail.com>
2022-12-03 23:19:49 +0900

    기본레스트API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { UsersController } from './users/users.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
   controllers: [MoviesController, UsersController],
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,35 +6,46 @@ import {
   Param,
   Patch,
   Post,
-  Put,
+  Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-dto';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
   @Get()
   getAll() {
-    return 'This will return all movies';
-  }
-
-  @Get('/:id')
-  getOne(@Param('id') movieId: string) {
-    return `this is id ${movieId}`;
+    return this.movieService.getAll();
   }
 
   @Post()
-  create(@Body() movieData) {
-    return movieData;
+  create(@Body() movieData: CreateMovieDto) {
+    return this.movieService.create(movieData);
+  }
+  /**
+   *
+   * @param searchingYear
+   * @returns
+   */
+  @Get('/search')
+  searcch(@Query('year') searchingYear: string) {
+    return `we are aearcning for ${searchingYear}`;
   }
 
   @Delete(`/:id`)
   remove(@Param('id') movieId: string) {
-    return `This will deleete a movie ${movieId}`;
+    return this.movieService.deleteOne(movieId);
   }
+
   @Patch(`/:id`)
   pathch(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+    return this.movieService.update(movieId, updateData);
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return this.movieService.getOne(movieId);
   }
 }
