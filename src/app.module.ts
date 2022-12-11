a35725a90adea444deaa46623c2e4a7c57252d0a
wanhwi.son <yeshtml5@gmail.com>
2022-12-11 23:25:31 +0900

    서비스등록및 컨트롤러 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { UsersController } from './users/users.controller';
 import { MoviesService } from './movies/movies.service';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule],
   controllers: [MoviesController, UsersController],
   providers: [MoviesService],
 })
