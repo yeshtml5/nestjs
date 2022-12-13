123d63d88d92d0afccf3972c37e394ab96924b31
wanhwi.son <yeshtml5@gmail.com>
2022-12-14 00:16:06 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: function (origin, callback) {
+      callback(null, true);
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
