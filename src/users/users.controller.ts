4e03d9b56a7a2957ef0e69acedaec1de22b6d741
wanhwi.son <yeshtml5@gmail.com>
2023-01-01 16:59:06 +0900

    updates files

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /**
+   *
+   * @param searchingYear
+   * @returns
+   */
+}
